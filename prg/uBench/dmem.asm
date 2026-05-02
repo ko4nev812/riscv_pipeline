ac7cda0cf2e17e50bcb376ad820aac887402d57e
verssy <verssy@mail.ru>
2026-05-02 13:59:08 +0700

    Fix dmem integration test

--- prg/uBench/dmem.asm
+++ prg/uBench/dmem.asm
@@ -1,33 +1,81 @@
+#------------------------------------------------------------------------------
+#    DMEM integration test (store insns: S[W|H|B], load insns: L[W|H|HU|B|BU])
+#------------------------------------------------------------------------------
+# Tests:
+#   1. Word tests: SW, LW
+#   2. Half tests: SH, LH, LHU
+#   3. Byte tests: SB, LB, LBU
+#------------------------------------------------------------------------------
+
+#--- common part for all tests (proposal)
+.eqv TEST_RESULT x31
+.eqv TEST_FAIL   2
+.eqv TEST_PASS   1
+
+#------------------------------------------------------------------------------
+
+.eqv DMEM_START_REG x10
+.eqv DMEM_LOAD_REG  x3
+.eqv EXPECT_REG     x4
+
+.data
+.align 4
+test_mem: .space 32
+
 .text
-# --- Инициализация ---
-li  x10, 0		# в rars заменить на lui	x10, 0x10000
-li  x1,  0x12345678   # Паттерн 1
-li  x2,  0x89ABCDEF   # Паттерн 2
+    la DMEM_START_REG, test_mem
 
-# --- Тест 1: WORD (32 бита) ---
-sw  x1, 0(x10)
-lw  x3, 0(x10)        # Ожидается: 0x12345678
+    # Init
+    li x1, 0x12345678
+    li x2, 0x89ABCDEF
 
-sw  x2, 4(x10)
-lw  x4, 4(x10)        # Ожидается: 0x89ABCDEF
+    # Test 1. Word tests: SW, LW
+    sw x1, 0(DMEM_START_REG)
+    lw DMEM_LOAD_REG, 0(DMEM_START_REG)         # Expect: 0x12345678
+    li EXPECT_REG, 0x12345678
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
+    sw x2, 4(DMEM_START_REG)
+    lw DMEM_LOAD_REG, 4(DMEM_START_REG)         # Expect: 0x89ABCDEF
+    li EXPECT_REG, 0x89ABCDEF
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
 
-# --- Тест 2: HALFWORD (16 бит) ---
-sh  x1, 8(x10)        # Запись 0x5678
-lh  x5, 8(x10)        # Ожидается: 0xFFFF5678 (sign-extend)
-lhu x6, 8(x10)        # Ожидается: 0x00005678 (zero-extend)
+    # Test 2. Half tests: SH, LH, LHU
+    sh x1, 8(DMEM_START_REG)                    # Store 0x5678
+    lh DMEM_LOAD_REG, 8(DMEM_START_REG)         # Expect: 0x00005678 (sign-extend)
+    li EXPECT_REG, 0x00005678
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
+    lhu DMEM_LOAD_REG, 8(DMEM_START_REG)        # Expect: 0x00005678 (zero-extend)
+    li EXPECT_REG, 0x00005678
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
+    sh x2, 12(DMEM_START_REG)                   # Store 0xCDEF
+    lh DMEM_LOAD_REG, 12(DMEM_START_REG)        # Expect: 0xFFFFCDEF
+    li EXPECT_REG, 0xFFFFCDEF
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
+    lhu DMEM_LOAD_REG, 12(DMEM_START_REG)       # Expect: 0x0000CDEF
+    li EXPECT_REG, 0x0000CDEF
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
 
-sh  x2, 12(x10)       # Запись 0xCDEF
-lh  x7, 12(x10)       # Ожидается: 0xFFFFCDEF
-lhu x8, 12(x10)       # Ожидается: 0x0000CDEF
+    # Test 3. Byte tests: SB, LB, LBU
+    sb x1, 16(DMEM_START_REG)                   # Store 0x78
+    lb DMEM_LOAD_REG, 16(DMEM_START_REG)        # Expect: 0x00000078
+    li EXPECT_REG, 0x00000078
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
+    lbu DMEM_LOAD_REG, 16(DMEM_START_REG)       # Expect: 0x00000078
+    li EXPECT_REG, 0x00000078
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
+    sb x2, 20(DMEM_START_REG)                   # Store 0xEF
+    lb DMEM_LOAD_REG, 20(DMEM_START_REG)        # Expect: 0xFFFFFFEF
+    li EXPECT_REG, 0xFFFFFFEF
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
+    lbu DMEM_LOAD_REG, 20(DMEM_START_REG)       # Expect: 0x000000EF
+    li EXPECT_REG, 0x000000EF
+    bne DMEM_LOAD_REG, EXPECT_REG, test_fail
+    j test_pass
 
-# --- Тест 3: BYTE (8 бит) ---
-sb  x1, 16(x10)       # Запись 0x78
-lb  x9,  16(x10)      # Ожидается: 0xFFFFFF78
-lbu x11, 16(x10)      # Ожидается: 0x00000078
+test_pass:
+    addi TEST_RESULT, x0, TEST_PASS
+    jal x0, test_pass
 
-sb  x2, 20(x10)       # Запись 0xEF
-lb  x12, 20(x10)      # Ожидается: 0xFFFFFFEF
-lbu x13, 20(x10)      # Ожидается: 0x000000EF
-
-# --- Конец (зацикливание) ---
-end: j end
+test_fail:
+    addi TEST_RESULT, x0, TEST_FAIL
+    jal x0, test_fail
